f228a2895af77d1c6b238fbe8942f9dc81f82af8
Ishani Srivastava <ishasri.1212@gmail.com>
2020-03-31 12:37:44 +0530

    Add logging for errors (#4)
    * Add logging
    
    * Add logging of variables using fields

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	config := utils.GetConfiguration()
 	utils.Init(config)
+	utils.Logger.Infof("Server running on port %s",config.ServerPort)
 	http.HandleFunc("/searchStudentMaterial", handler.SearchStudentMaterial)
 	http.HandleFunc("/searchTeacherMaterial", handler.SearchTeacherMaterial)
 	log.Fatal(http.ListenAndServe(config.ServerPort, nil))
