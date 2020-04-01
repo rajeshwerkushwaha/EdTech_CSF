6afc987c8dcc7dbaf50f1b637b4d38ff98ffb3ec
ishanisri <ishasri.1212@gmail.com>
2020-04-01 10:21:39 +0530

    Clean the processed data to remove duplicate rows

--- convert_data.py
+++ convert_data.py
@@ -1,13 +1,17 @@
 import csv
 import sys
 
-if(len(sys.argv) != 3):
+if(len(sys.argv) != 4):
     sys.exit("Error: Not enough arguments. There should be 2 arguments for input filename and output filename respectively")
 input_filename = sys.argv[1] #'Website_CBSE_EdTech_caterorization.csv'
 output_filename = sys.argv[2] #'final_edtech_csf.csv'
 
 input_data = []
 content_description_col = 2 # can be changed based on its position in the input file.
+if sys.argv[3] =="Student":
+    max_col = 9
+else:
+    max_col = 7
 
 
 def create_multiple_rows(input_row,cell_data,multi_count):
@@ -17,6 +21,8 @@ def create_multiple_rows(input_row,cell_data,multi_count):
         position = cell_data.find(",")
         cell_data_list.append(cell_data[:position])
         cell_data = cell_data[position+1:]
+    if cell_data != '':
+        cell_data_list.append(cell_data)
     for data in cell_data_list:
         row_data = []
         for count in range(len(input_row)):
@@ -24,34 +30,45 @@ def create_multiple_rows(input_row,cell_data,multi_count):
                 row_data.append(data.strip())
             else:
                 row_data.append(input_row[count].strip())
-        print(row_data)
         multi_rows.append(row_data)
-    print(multi_rows)
     return multi_rows
 
 
-row_count = -1
-
-with open(input_filename,'r') as csvfile:
-    filereader = csv.reader(csvfile,delimiter=',')
-
+def process_file(filereader):
+    processing_flag = 0
+    processed_data = []
+    row_count = -1
     for input_row in filereader:
         row_count = row_count+1
         row_data = []
         for count in range(len(input_row)):
             cell_data = input_row[count]
             if cell_data.count(",") != 0 and count != content_description_col and row_count != 0:
-                multi_rows = create_multiple_rows(input_row,cell_data,count)
+                processing_flag = 1
+                multi_rows = create_multiple_rows(input_row,cell_data.strip(),count)
                 for row in multi_rows:
-                    input_data.append(row)
+                    processed_data.append(row)
             else:
                 row_data.append(input_row[count].strip())
-        input_data.append(row_data)
+        processed_data.append(row_data)
+    return processed_data,processing_flag
 
-print(input_data)
+
+with open(input_filename,'r') as csvfile:
+    filereader = csv.reader(csvfile,delimiter=',')
+    input_data = filereader
+    processing_flag = 1
+    while processing_flag == 1:
+        input_data,processing_flag = process_file(input_data)
+    cleaned_data = []
+    for row in input_data:
+        if row not in cleaned_data:
+            if len(row) == max_col:
+                cleaned_data.append(row)
+
 
 with open(output_filename, mode='w+') as final_file:
     file_writer = csv.writer(final_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    file_writer.writerows(input_data)
+    file_writer.writerows(cleaned_data)
 
 print("Done")
