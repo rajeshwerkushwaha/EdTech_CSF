fccb3179d0752db70cb38b1d984906ff46be9012
Akshay Pai <paiakshay998@gmail.com>
2020-04-02 12:59:00 +0530

    Multiple link support on frontend

--- public/js/script.js
+++ public/js/script.js
@@ -47,10 +47,44 @@ uniqueList = (dataList) => {
   return dataList.filter((item, i, ar) => ar.indexOf(item) === i);
 }
 
+// parseLink takes a comma separated value and returns [webLink, mobileLink] array 
+parseLink = (linkString) => {
+  let linkArray = linkString.split(",");
+  if (linkArray.length == 1) {
+    return [linkString, ""];
+  }
+  let webLink = (linkArray[0].find("play.google.com") == -1) ? linkArray[0]: linkArray[1];
+  let mobileLink = (linkArray[0] == webLink) ? linkArray[1]: linkArray[0];
+  return [webLink, mobileLink];
+}
+
+convertDevice = (device) => {
+  if (device == "1")
+    return "Smartphone";
+  else if (device == "2")
+    return "Laptop";
+  else
+    return "Smartphone,Laptop";
+}
+
+getLinkToUse = (device, availableLinks) => {
+  if (device == "1")
+    return availableLinks[1];
+  else if (device == "2")
+    return availableLinks[0];
+  else {
+    if (screen.width < 600)
+      return availableLinks[1];
+    return availableLinks[0];
+  }
+}
+
 populateTeacherData = (data) => {
   let title_to_usecase = {};
   let title_to_description = {};
   let title_to_link = {};
+  let title_to_cost = {};
+  let title_to_device = {};
 
   for (obj of data) {
     let content_title = obj['content_title'];
@@ -58,22 +92,26 @@ populateTeacherData = (data) => {
     if (!(content_title in title_to_usecase)) {
       title_to_usecase[content_title] = [];
       title_to_description[content_title] = obj['content_description'];
-      title_to_link[content_title] = obj['content_link'];
+      title_to_link[content_title] = parseLink(obj['content_link']);
+      title_to_cost[content_title] = obj['cost'];
+      title_to_device[content_title] = obj['device'];
     }
     title_to_usecase[content_title].push(obj['use_case']);
   }
   for (content_title in title_to_usecase) {
     let content_usecase = uniqueList(title_to_usecase[content_title]);
+    let linkToUse = getLinkToUse(title_to_device[content_title], title_to_link[content_title]);
+
     let element = `
-      <div onclick="openInNewTab('${title_to_link[content_title]}');" class="row custom-data-box custom-margin custom-pointer">
+      <div onclick="openInNewTab('${linkToUse}');" class="row custom-data-box custom-margin custom-pointer">
         <div class="row custom-forced-width"><div class="column"><h2>${content_title}</h2></div></div>
         <div class="row custom-forced-width">${title_to_description[content_title]}</div>
         <div class="row custom-forced-width">
           <div class="ui stackable four column grid">
             <div class="column">${content_usecase.join(", ")}</div>
             <div class="column"></div>
-            <div class="column"></div>
-            <div class="column"></div>
+            <div class="column">${title_to_cost[content_title]}</div>
+            <div class="column">${convertDevice(title_to_device[content_title])}</div>
           </div>
         </div>
       </div>`;
@@ -87,6 +125,8 @@ populateStudentData = (data) => {
   let title_to_grade = {};
   let title_to_description = {};
   let title_to_link = {};
+  let title_to_cost = {};
+  let title_to_device = {};
 
   for (obj of data) {
     let content_title = obj['content_title'];
@@ -94,7 +134,9 @@ populateStudentData = (data) => {
     if (!(content_title in title_to_subject)) {
       title_to_subject[content_title] = [];
       title_to_description[content_title] = obj['content_description'];
-      title_to_link[content_title] = obj['content_link'];
+      title_to_link[content_title] = parseLink(obj['content_link']);
+      title_to_cost[content_title] = obj['cost'];
+      title_to_device[content_title] = obj['device'];
     }
     title_to_subject[content_title].push(obj['subject']);
 
@@ -110,17 +152,18 @@ populateStudentData = (data) => {
   for (content_title in title_to_subject) {
     let content_grade = uniqueList(title_to_grade[content_title]);
     let content_subject = uniqueList(title_to_subject[content_title]);
+    let linkToUse = getLinkToUse(title_to_device[content_title], title_to_link[content_title]);
 
     let element = `
-    <div onclick="openInNewTab('${title_to_link[content_title]}');" class="row custom-data-box custom-margin custom-pointer">
+    <div onclick="openInNewTab('${linkToUse}');" class="row custom-data-box custom-margin custom-pointer">
       <div class="row custom-forced-width"><div class="column"><h2>${content_title}</h2></div></div>
       <div class="row custom-forced-width">${title_to_description[content_title]}</div>
       <div class="row custom-forced-width">
         <div class="ui stackable four column grid">
           <div class="column">Grade ${content_grade.join(", ")}</div>
           <div class="column">${content_subject.join(", ")}</div>
-          <div class="column"></div>
-          <div class="column"></div>
+          <div class="column">${title_to_cost[content_title]}</div>
+          <div class="column">${convertDevice(title_to_device[content_title])}</div>
         </div>
       </div>
     </div>
